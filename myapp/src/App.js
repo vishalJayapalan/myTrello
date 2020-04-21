a6f4a230fd8ea7b125c238c269d6b1a5477eacba
vishal jayapalan <vishal935@gmail.com>
2020-04-21 09:11:38 +0530

    after routing

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,17 +2,20 @@ import React from 'react'
 import Navbar from './components/navbar.js'
 import Boards from './components/boards.js'
 import Lists from './components/lists.js'
+import Home from './components/home.js'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App () {
   return (
     <div className='App'>
       <Navbar />
-      <div>
-        <Boards />
-      </div>
-      <div>
-        <Lists />
-      </div>
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/boards' component={Boards} />
+          <Route path='/lists' component={Lists} />
+        </Switch>
+      </Router>
     </div>
   )
 }

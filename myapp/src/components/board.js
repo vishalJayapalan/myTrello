fb15ba0d9dafb3cd6ae1886edbbcfcfa4c53801e
vishal jayapalan <vishal935@gmail.com>
2020-04-22 09:22:56 +0530

    working on board

---
+++ myapp/src/components/board.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Board (props) {
+  return (
+    <div className='boardcontainer'>
+      <div className='boardbox'>
+        <p>Create New Board..</p>
+      </div>
+    </div>
+  )
+}

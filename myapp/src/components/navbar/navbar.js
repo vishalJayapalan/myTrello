613b4b81fa54a373d4e1e7dcfeac216e42ace716
vishal jayapalan <vishal935@gmail.com>
2020-12-01 19:32:31 +0530

    minor responsiveness added

--- myapp/src/components/navbar/navbar.js
+++ myapp/src/components/navbar/navbar.js
@@ -18,7 +18,7 @@ export default function Navbar () {
       const jsonData = await response.json()
 
       // const jsonData = await response.json()
-      console.log('jsonData', jsonData)
+      // console.log('jsonData', jsonData)
       setUser(jsonData)
     }
   }
